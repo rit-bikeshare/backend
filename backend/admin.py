5147c7fa114bb84a0da98da8d2ef2c7716809d96
Scott Ketelaar <git@scottketelaar.com>
2017-10-16 15:06:23 -0400

    Add admin interface

--- backend/admin.py
+++ backend/admin.py
@@ -1,3 +1,149 @@
+from django import forms
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+@admin.register(models.BikeRack)
+class BikeRackAdmin(admin.ModelAdmin):
+	# Controls the summary view
+	list_display = ('name', 'description', 'lon', 'lat', 'id')
+	search_fields = ('name', 'id')
+	ordering = ('name',)
+
+	# Controls the add/change pages
+	prepopulated_fields = {"id": ("name",)}
+	fieldsets = (
+		('General', {
+			'fields': ('name', 'description') 
+		}),
+		('Location', {
+			'fields': (('lon', 'lat'),)
+		}),
+		('Advanced', {
+			'fields': ('id',)
+		})
+	)
+
+@admin.register(models.Rental)
+class RentalAdmin(admin.ModelAdmin):
+	# Controls the summary view
+	list_display = ('id', 'renter', 'bike', 'rented_at', 'should_return_at', 'returned_at', 'is_complete', 'is_late')
+	search_fields = ('renter__username', 'bike__id')
+	ordering = ('rented_at',)
+
+	# Controls the add/change pages
+	fieldsets = (
+		('General', {
+			'fields': ('renter', 'bike') 
+		}),
+		('Times', {
+			'fields': ('rented_at','should_return_at', 'returned_at')
+		})
+	)
+
+	def get_changeform_initial_data(self, request):
+		d = super().get_changeform_initial_data(request)
+		start = models.Rental.get_rental_start()
+		end = models.Rental.get_rental_end(start)
+		d.update({
+			'rented_at': start,
+			'should_return_at': end
+		})
+
+		return d
+
+@admin.register(models.Bike)
+class BikeAdmin(admin.ModelAdmin):
+	# Controls the summary view
+	list_display = ('id', 'bikerack', 'is_rented', 'visible')
+	search_fields = ('id',)
+	ordering = ('id',)
+
+	# Controls the add/change pages
+	fieldsets = (
+		('General', {
+			'fields': ('visible', 'bikerack') 
+		}),
+		('Advanced', {
+			'fields': ('id',)
+		})
+	)
+
+
+class MaintenanceReportForm(forms.ModelForm):
+	class Meta:
+		model = models.MaintenanceReport
+		exclude = []
+
+	# Create a field to allow selecting of damages
+	# The actual query will be constructed dynamically
+	damages = forms.ModelMultipleChoiceField(queryset=models.DamageReport.objects.none())
+
+	def __init__(self, *args, **kwargs):
+		super().__init__(*args, **kwargs)
+		if self.instance:
+			self.fields['damages'].queryset = models.DamageReport.objects.filter(resolved_by=None, bike=self.instance.bike)
+
+	def save(self, *args, **kwargs):
+		# FIXME: 'commit' argument is not handled
+		# TODO: Wrap reassignments into transaction
+		# NOTE: Previously assigned Foos are silently reset
+		instance = super().save(commit=False)
+		self.cleaned_data['damages'].update(resolved_by=instance)
+		return instance
+
+
+@admin.register(models.MaintenanceReport)
+class MaintenanceReportAdmin(admin.ModelAdmin):
+	# Controls the summary view
+	list_display = ('id', 'bike', 'created_at')
+	search_fields = ('bike', 'comments')
+	ordering = ('-id',)
+
+	# Controls the add/change pages
+	form = MaintenanceReportForm
+	fieldsets = (
+		('General', {
+			'fields': ('bike',) 
+		}),
+		('Maintenance', {
+			'fields': ('damages', 'comments',)
+		})
+	)
+
+@admin.register(models.DamageType)
+class DamageTypeAdmin(admin.ModelAdmin):
+	# Controls the summary view
+	list_display = ('id', 'name', 'description')
+	search_fields = ('name', 'id')
+	ordering = ('name',)
+
+	# Controls the add/change pages
+	prepopulated_fields = {"id": ("name",)}
+	fieldsets = (
+		('General', {
+			'fields': ('name', 'description') 
+		}),
+		('Advanced', {
+			'fields': ('id',)
+		})
+	)
+
+@admin.register(models.DamageReport)
+class DamageReportAdmin(admin.ModelAdmin):
+	# Controls the summary view
+	list_display = ('id', 'bike', 'damage_type', 'reporter')
+	search_fields = ('bike', 'damage_type')
+	ordering = ('-id',)
+
+	# Controls the add/change pages
+	fieldsets = (
+		('General', {
+			'fields': ('reporter', 'bike') 
+		}),
+		('Damage', {
+			'fields': ('damage_type', 'comments')
+		}),
+		('Resolution', {
+			'fields': ('resolved_by',)
+		})
+	)
--- backend/models.py
+++ backend/models.py
@@ -1,1 +1,101 @@
+from django.conf import settings
 from django.db import models
+from django.utils import timezone
+from datetime import timedelta
+
+class BikeRack(models.Model):
+	id = models.SlugField(primary_key=True, help_text='The ID for this bike rack. This should match the QR code on the rack')	
+	name = models.CharField(max_length=50, blank=False, unique=True, help_text='Friendly name of the bike rack such as "Grace Watson Rack"')
+	description = models.CharField(max_length=255, blank=False, help_text='Description of where the rack is located')
+	lon = models.DecimalField(max_digits=9, decimal_places=6, help_text='Longitude of the bike rack\'s coordinates')
+	lat = models.DecimalField(max_digits=9, decimal_places=6, help_text='Latitude of the bike rack\'s coordinates')
+
+	def __str__(self):
+		return self.name
+	#enddef
+
+
+class Bike(models.Model):
+	id = models.SlugField(primary_key=True, help_text='ID for the bike. This should match the QR code on the bike')
+	visible = models.BooleanField(default=True, help_text='Determines if this bike is rentable. Use this instead of deleting bikes')
+	bikerack = models.ForeignKey(BikeRack, help_text='The most recent location of the bike.')
+
+	def get_current_rental(self):
+		# a bike is rented if the most recent rental started but hasn't completed
+		try:
+			r = Rental.objects.filter(bike=self).latest('rented_at').first()
+			if not r or r.returned_at == None: return None
+			return r
+		except Exception as e:
+			return None
+	#enddef
+
+	def is_rented(self):
+		return self.get_current_rental() is not None
+	#enddef
+	is_rented.boolean = True
+
+	def __str__(self):
+		return self.id
+	#enddef
+
+
+class Rental(models.Model):
+	renter = models.ForeignKey(settings.AUTH_USER_MODEL, help_text='User who rented the bike')
+	bike = models.ForeignKey(Bike, help_text='The bike they rented')
+	rented_at = models.DateTimeField(help_text='When the rental began')
+	should_return_at = models.DateTimeField(help_text='When the renter was supposed to return the bike by')
+	returned_at = models.DateTimeField(null=True, blank=True, help_text='When the bike was actually returned')
+
+	@classmethod
+	def get_rental_start(cls):
+		return timezone.now()
+	#enddef
+
+	@classmethod
+	def get_rental_end(cls, start, duration=timedelta(1)):
+		return start + duration
+	#enddef
+
+	def is_complete(self):
+		return self.returned_at is not None
+	#enddef
+	is_complete.boolean = True
+
+	def is_late(self):
+		r_date = self.returned_at if self.is_complete() else timezone.now()
+		return r_date > self.should_return_at
+	#enddef
+	is_late.boolean = True
+
+	def __str__(self):
+		return str(self.id)
+
+class MaintenanceReport(models.Model):
+	bike = models.ForeignKey(Bike, help_text='Bike that is the subject of this report')
+	created_at = models.DateTimeField(auto_now_add=True, help_text='When this report was entered')
+	comments = models.TextField(help_text='Any additional comments about this bike')
+
+
+class DamageType(models.Model):
+	id = models.SlugField(primary_key=True, help_text='The ID for this damage type')
+	name = models.CharField(max_length=50, help_text='Friendly name of this damage, such as "Flat tire"')
+	description = models.CharField(max_length=255, help_text='Description of what this damage type means')
+
+
+	def __str__(self):
+		return self.name
+	#enddef
+
+
+class DamageReport(models.Model):
+	reporter = models.ForeignKey(settings.AUTH_USER_MODEL, help_text='The user who reported the damage')
+	bike = models.ForeignKey(Bike, help_text='The bike that was damaged')
+	damage_type = models.ForeignKey(DamageType, help_text='The type of damage done')
+	reported_at = models.DateTimeField(auto_now_add=True, help_text='When the damage was reported')
+	comments = models.TextField(help_text='Additional comments about the damage')
+	resolved_by = models.ForeignKey(MaintenanceReport, blank=True, null=True, help_text='The maintenance report that resolved this damage')
+
+	def __str__(self):
+		return '{} - {}'.format(self.bike.id, self.damage_type.name)
+	#enddef
