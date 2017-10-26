59246fa0bdd250ff277aee1151cffb1b8258f2b5
Megan Ketelaar <git@meganketelaar.com>
2017-10-26 14:39:57 -0400

    Add checkin

--- bikeshare/serializers.py
+++ bikeshare/serializers.py
@@ -13,3 +13,23 @@ class RentalSerializer(serializers.ModelSerializer):
 
 class CheckoutRequestSerializer(serializers.Serializer):
 	bike = serializers.SlugField()
+
+class CheckInRequestSerializer(serializers.Serializer):
+	bike = serializers.SlugField()
+
+	# Can specify bikerack OR lat/lon
+	bikerack = serializers.SlugField(required=False)
+	lat = serializers.DecimalField(max_digits=9, decimal_places=6, required=False)
+	lon = serializers.DecimalField(max_digits=9, decimal_places=6, required=False)
+
+	def validate(self, data):
+		# Ensure that if we got lat, we also got lon
+		if ('lat' in data) != ('lon' in data): raise serializers.ValidationError('Both lat and lon must be specified')
+
+		if not (('lat' in data) or ('bikerack' in data)): raise serializers.ValidationError('Must specify either bikerack or lat/lon')
+
+		# If we got bikerack, it's an error to supply lat/lon
+		if ('lat' in data) and ('bikerack' in data): raise serializers.ValidationError('Cannot specify both bikerack and lat/lon')
+
+		# All good
+		return data
--- bikeshare/views.py
+++ bikeshare/views.py
@@ -3,20 +3,16 @@ from django.db import transaction
 from django.db.models import Case, When, F
 from django.shortcuts import get_object_or_404
 from django.views.decorators.csrf import csrf_exempt
+from django.utils import timezone
 from rest_framework.decorators import api_view, permission_classes
-from rest_framework.exceptions import APIException
 from rest_framework import permissions, status
 from rest_framework.response import Response
 
-from . import models, serializers
+from . import models, serializers, exceptions
 
 def permission_required(perm):
 	return __permission_required(perm, raise_exception=True)
 
-class AlreadyRentedException(APIException):
-	status_code = status.HTTP_409_CONFLICT
-	default_detail = 'This bike is already rented!'
-	defult_code = 'already_rented'
 
 @csrf_exempt
 @api_view(['POST'])
@@ -26,27 +22,62 @@ def checkout(request):
 	serializer = serializers.CheckoutRequestSerializer(data=request.data)
 	if not serializer.is_valid(): return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-	# Get the bike
-	bike = get_object_or_404(models.Bike, id=serializer.validated_data['bike'])
-
-	# Make a new rental. For now the duration is hardcoded
-	rental_start = models.Rental.get_rental_start()
-	rental_end = models.Rental.get_rental_end(rental_start)
-
-	rental = models.Rental(renter=request.user, bike=bike, rented_at=rental_start, should_return_at=rental_end)
-
-	# Try to save it, failing if this bike is already rented
 	# Start a new transaction
 	with transaction.atomic():
-		rental.save()
-		# Now try to update the bike's rental, but only if it's not already rented.
-		num_updated = models.Bike.objects.filter(id=bike.id, current_rental=None).update(current_rental=rental)
+		# Get the bike with select_for_update to lock it for the duration of the transaction
+		bike = get_object_or_404(models.Bike.objects.select_for_update(), id=serializer.validated_data['bike'])
 
-		if num_updated != 1:
-			raise AlreadyRentedException()
+		# Can't rent a bike twice
+		if bike.current_rental != None:
+			raise exceptions.AlreadyRentedException()
+
+		# Make a new rental. For now the duration is hardcoded
+		rental_start = models.Rental.get_rental_start()
+		rental_end = models.Rental.get_rental_end(rental_start)
+
+		rental = models.Rental.objects.create(renter=request.user, bike=bike, rented_at=rental_start, should_return_at=rental_end)
+
+		bike.current_rental = rental
+		bike.save()
 	#end transaction
 
 	return Response(serializers.RentalSerializer(rental).data, status=status.HTTP_201_CREATED)
 #end checkout
 
+@csrf_exempt
+@api_view(['POST'])
+@permission_classes((permissions.IsAuthenticated,))
+# Not requiring bike.checkout perms here otherwise a user could get stuck with a checked out bike forever
+def check_in(request):
+	serializer = serializers.CheckInRequestSerializer(data=request.data)
+	if not serializer.is_valid(): return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+	bikerack_id = serializer.validated_data.get('bikerack', None)
+	if bikerack_id:
+		# They gave us a bikerack, so look up its coords
+		lat, lon = get_object_or_404(models.BikeRack.objects.values_list('lat', 'lon'), id=bikerack_id)
+	else:
+		# Gave us coords directly
+		lat, lon = serializer.validated_data['lat'], serializer.validated_data['lon']
+	#endif
+
+	with transaction.atomic():
+		# Load the bike (if present)
+		bike = get_object_or_404(models.Bike.objects.select_for_update(), id=serializer.validated_data['bike'])
+		rental = bike.current_rental
+
+		if rental == None: raise exceptions.NotRentedException()
+		if rental.renter != request.user: raise exceptions.NotYourRentalException()
+
+		# Set the rental's actual end to now
+		rental.returned_at = timezone.now()
+		rental.save()
+
+		# clear the rental and update coords
+		bike.current_rental = None
+		bike.lat, bike.lon = lat, lon
+		bike.save()
+	#end transaction
+
+	return Response(serializers.RentalSerializer(rental).data, status=status.HTTP_200_OK)
+#end checkin	
