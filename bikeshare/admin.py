577cf9d0d5359852221b7331289256a69f1ab710
Scott Ketelaar <git@scottketelaar.com>
2017-10-27 15:47:10 -0400

    Extend auth so we can deny privs to users

--- bikeshare/admin.py
+++ bikeshare/admin.py
@@ -1,6 +1,22 @@
 from django import forms
 from django.contrib import admin
+from django.utils.translation import gettext, gettext_lazy as _
+
 from . import models
+
+from shib_auth.admin import ShibUserAdmin
+
+@admin.register(models.BikeshareUser)
+class BikeshareUserAdmin(ShibUserAdmin):
+	fieldsets = (
+        (None, {'fields': ('username',)}),
+        (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser',
+                                       'groups', 'user_permissions', 'user_denied_permissions')}),
+        (_('Important dates'), {'fields': ('last_login',)}),
+    )
+	
+	filter_horizontal = ('groups', 'user_permissions', 'user_denied_permissions')
+
 
 @admin.register(models.BikeRack)
 class BikeRackAdmin(admin.ModelAdmin):
---
+++ extended_auth/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class ExtendedAuthConfig(AppConfig):
+    name = 'extended_auth'
