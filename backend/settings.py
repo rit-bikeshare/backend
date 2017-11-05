ffd45e17f31542d1113eeb2852c94883c406f334
Megan Ketelaar <git@meganketelaar.com>
2017-11-04 23:11:16 -0400

    Convert snake case to camel

--- backend/settings.py
+++ backend/settings.py
@@ -31,88 +31,88 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'django.contrib.gis',
+	'django.contrib.admin',
+	'django.contrib.auth',
+	'django.contrib.contenttypes',
+	'django.contrib.sessions',
+	'django.contrib.messages',
+	'django.contrib.staticfiles',
+	'django.contrib.gis',
 
-    'constance', # DB-backed settings
-    'constance.backends.database',
+	'constance', # DB-backed settings
+	'constance.backends.database',
 
-    'admin_reorder',
-    
-    'rest_framework',
-    
-    'shib_auth',
-    
-    'bikeshare'
+	'admin_reorder',
+	
+	'rest_framework',
+	
+	'shib_auth',
+	
+	'bikeshare'
 ]
 
 MIDDLEWARE = [
-    'admin_reorder.middleware.ModelAdminReorder',
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'shib_auth.middleware.ShibbolethRemoteUserMiddleware',
-    'bikeshare.middleware.MaintenanceInterceptorMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+	'admin_reorder.middleware.ModelAdminReorder',
+	'django.middleware.security.SecurityMiddleware',
+	'django.contrib.sessions.middleware.SessionMiddleware',
+	'django.middleware.common.CommonMiddleware',
+	'django.middleware.csrf.CsrfViewMiddleware',
+	'django.contrib.auth.middleware.AuthenticationMiddleware',
+	'shib_auth.middleware.ShibbolethRemoteUserMiddleware',
+	'bikeshare.middleware.MaintenanceInterceptorMiddleware',
+	'django.contrib.messages.middleware.MessageMiddleware',
+	'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
 ROOT_URLCONF = 'backend.urls'
 
 TEMPLATES = [
-    {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-            ],
-        },
-    },
+	{
+		'BACKEND': 'django.template.backends.django.DjangoTemplates',
+		'DIRS': [],
+		'APP_DIRS': True,
+		'OPTIONS': {
+			'context_processors': [
+				'django.template.context_processors.debug',
+				'django.template.context_processors.request',
+				'django.contrib.auth.context_processors.auth',
+				'django.contrib.messages.context_processors.messages',
+			],
+		},
+	},
 ]
 
 WSGI_APPLICATION = 'backend.wsgi.application'
 
 ADMIN_REORDER = (
-    'bikeshare',
-    'constance',
-    # Cross-linked models
-    {'app': 'auth', 'models': ('auth.Group', 'bikeshare.BikeshareUser')},
+	'bikeshare',
+	'constance',
+	# Cross-linked models
+	{'app': 'auth', 'models': ('auth.Group', 'bikeshare.BikeshareUser')},
 )
 
 # Setting editable through admin interface
 from collections import OrderedDict
 CONSTANCE_CONFIG = OrderedDict([
-    ('ENABLE_DROP_ANYWHERE', (False, 'Turn on or off backend support for drop anywhere')),
+	('ENABLE_DROP_ANYWHERE', (False, 'Turn on or off backend support for drop anywhere')),
 
-    ('ALLOW_CHECKOUT', (True, 'Should the app allow users to check out a bike')),
-    ('CHECKOUT_DISALLOWED_MESSAGE', ('Bikeshare checkout has been closed until Spring. Check back later!',
-        'Message to display when checkout is disabled and user attempts checkout'
-    )),
-    ('MAINTENANCE_MODE', (False, 'Set to True to disable system')),
-    ('MAINTENANCE_MESSAGE', ('Bikeshare is currently unavailable. Please try again later.',
-        'Message to display when a user accesses the app in mainenance mode'
-    )),
+	('ALLOW_CHECKOUT', (True, 'Should the app allow users to check out a bike')),
+	('CHECKOUT_DISALLOWED_MESSAGE', ('Bikeshare checkout has been closed until Spring. Check back later!',
+		'Message to display when checkout is disabled and user attempts checkout'
+	)),
+	('MAINTENANCE_MODE', (False, 'Set to True to disable system')),
+	('MAINTENANCE_MESSAGE', ('Bikeshare is currently unavailable. Please try again later.',
+		'Message to display when a user accesses the app in mainenance mode'
+	)),
 
-    ('ADMIN_DEFAULT_LAT', (43.08397722062759, 'Default lat of map in admin interface')),
-    ('ADMIN_DEFAULT_LON', (-77.67605446140749, 'Default lon of map in admin interface')),
-    ('ADMIN_DEFAULT_ZOOM', (15, 'Default zoom of map in admin interface')),
+	('ADMIN_DEFAULT_LAT', (43.08397722062759, 'Default lat of map in admin interface')),
+	('ADMIN_DEFAULT_LON', (-77.67605446140749, 'Default lon of map in admin interface')),
+	('ADMIN_DEFAULT_ZOOM', (15, 'Default zoom of map in admin interface')),
 ])
 CONSTANCE_CONFIG_FIELDSETS = {
-    'Features': ('ENABLE_DROP_ANYWHERE',),
-    'Maintenance Settings': ('ALLOW_CHECKOUT', 'CHECKOUT_DISALLOWED_MESSAGE', 'MAINTENANCE_MODE', 'MAINTENANCE_MESSAGE'),
-    'Map Settings': ('ADMIN_DEFAULT_LAT', 'ADMIN_DEFAULT_LON', 'ADMIN_DEFAULT_ZOOM')
+	'Features': ('ENABLE_DROP_ANYWHERE',),
+	'Maintenance Settings': ('ALLOW_CHECKOUT', 'CHECKOUT_DISALLOWED_MESSAGE', 'MAINTENANCE_MODE', 'MAINTENANCE_MESSAGE'),
+	'Map Settings': ('ADMIN_DEFAULT_LAT', 'ADMIN_DEFAULT_LON', 'ADMIN_DEFAULT_ZOOM')
 }
 CONSTANCE_BACKEND = 'constance.backends.database.DatabaseBackend'
 
@@ -121,32 +121,40 @@ CONSTANCE_BACKEND = 'constance.backends.database.DatabaseBackend'
 
 SPATIALITE_LIBRARY_PATH = 'mod_spatialite'
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.contrib.gis.db.backends.spatialite',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+	'default': {
+		'ENGINE': 'django.contrib.gis.db.backends.spatialite',
+		'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+	}
 }
 
 REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': ('bikeshare.rest_auth.NoCsrfSessionAuthentication',)
+	'DEFAULT_AUTHENTICATION_CLASSES': ('bikeshare.rest_auth.NoCsrfSessionAuthentication',),
+
+	'DEFAULT_RENDERER_CLASSES': (
+		'djangorestframework_camel_case.render.CamelCaseJSONRenderer',
+	),
+
+	'DEFAULT_PARSER_CLASSES': (
+		'djangorestframework_camel_case.parser.CamelCaseJSONParser',
+	),
 }
 
 AUTHENTICATION_BACKENDS = (
-    'bikeshare.backends.BikeshareRemoteUserBackend',
+	'bikeshare.backends.BikeshareRemoteUserBackend',
 )
 
 SHIB_USERNAME_ATTRIB_NAME = 'uid'
 SHIB_ATTRIBUTE_MAP = {
-    "uid": (True, "username"),
-    "sn": (False, "last_name"),
-    "givenName": (False, "first_name")
+	"uid": (True, "username"),
+	"sn": (False, "last_name"),
+	"givenName": (False, "first_name")
 }
 
 SHIB_MOCK = True
 SHIB_MOCK_ATTRIBUTES = {
-    'uid': 'test',
-    'sn' : 'McTestface',
-    'first_name': 'Testy'
+	'uid': 'test',
+	'sn' : 'McTestface',
+	'first_name': 'Testy'
 }
 
 AUTH_USER_MODEL = 'bikeshare.BikeshareUser'
