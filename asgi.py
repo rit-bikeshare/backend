b3d0677eafb7f0bdb8ba2eb31a0fee92d354490d
Megan Ketelaar <git@meganketelaar.com>
2018-04-20 18:01:38 -0400

    Fix ASGI error

--- asgi.py
+++ asgi.py
@@ -1,13 +1,16 @@
-from django.urls import include
-
-from channels.routing import ProtocolTypeRouter, URLRouter
-import app.routing
-
 import os
 
 if 'DJANGO_SETTINGS_MODULE' not in os.environ:
 	from django.core.exceptions import ImproperlyConfigured
 	raise ImproperlyConfigured('You must define the environment variable DJANGO_SETTINGS_MODULE.')
+
+import django
+django.setup()
+
+from django.urls import include
+
+from channels.routing import ProtocolTypeRouter, URLRouter
+import app.routing
 
 application = ProtocolTypeRouter({
 	# (http->django views is added by default)
